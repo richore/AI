c448c5ffb46fd93c48ddfacf8d71cad10c22c2a7
richore <liuxianzhu@live.com>
2020-08-30 08:01:53 +0800

    First commit

---
+++ AIDemo/AIDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AIDemo
+//
+//  Created by liu on 2020/8/30.
+//  Copyright © 2020 liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
